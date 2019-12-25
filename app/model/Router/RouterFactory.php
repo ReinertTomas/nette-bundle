fb8b57ac9df5c99e5f562c3ebda0ed709a0dbb44
Tomas Reinert <reinerttomas@gmail.com>
2019-12-25 21:53:19 +0100

    add Front

--- app/model/Router/RouterFactory.php
+++ app/model/Router/RouterFactory.php
@@ -8,10 +8,27 @@ use Nette\Application\Routers\RouteList;
 
 final class RouterFactory
 {
+    /**
+     * @phpstan-return RouteList><Route>
+     */
     public function create(): RouteList
     {
         $router = new RouteList;
-        $router->addRoute('<presenter>/<action>', 'Homepage:default');
+
+        $this->buildFront($router);
+
         return $router;
+    }
+
+    /**
+     * @phpstan-param RouteList<Route> $router
+     * @phpstan-return RouteList<Route>
+     */
+    protected function buildFront(RouteList $router): RouteList
+    {
+        $list = new RouteList('Front');
+        $list->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
+
+        return $router->add($list);
     }
 }
