cff498c1f8a603a95e12776e43063b74b63fd000
Tomas Reinert <reinerttomas@gmail.com>
2019-12-26 16:26:14 +0100

    remove error layout

--- app/modules/Front/Error4xx/Error4xxPresenter.php
+++ app/modules/Front/Error4xx/Error4xxPresenter.php
@@ -3,24 +3,9 @@ declare(strict_types=1);
 
 namespace App\Modules\Front\Error4xx;
 
-use ReflectionClass;
 use App\Modules\Base\BaseError4xxPresenter;
 
 final class Error4xxPresenter extends BaseError4xxPresenter
 {
-    public function formatLayoutTemplateFiles(): array
-    {
-        $list = parent::formatLayoutTemplateFiles();
 
-        $rf = new ReflectionClass(static::class);
-        $dir = dirname($rf->getFileName());
-
-        $layout = "$dir/../templates/@layout.latte";
-
-        if (is_file($layout)) {
-            array_unshift($list, $layout);
-        }
-
-        return  $list;
-    }
 }
