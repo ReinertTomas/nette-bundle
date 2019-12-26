44fae0ee361e967a683e37a92c9bf50c7566aead
Tomas Reinert <reinerttomas@gmail.com>
2019-12-26 16:38:29 +0100

    add Admin

---
+++ app/modules/Admin/BaseAdminPresenter.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Modules\Admin;
+
+use App\Modules\Base\SecuredPresenter;
+use App\Modules\Base\UnsecuredPresenter;
+
+abstract class BaseAdminPresenter extends UnsecuredPresenter
+{
+
+}
---
+++ app/modules/Admin/Home/HomePresenter.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Modules\Admin\Home;
+
+use App\Modules\Admin\BaseAdminPresenter;
+
+class HomePresenter extends BaseAdminPresenter
+{
+
+}
