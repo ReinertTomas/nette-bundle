e5d01fc695a10cdce7ea7d7b6e60f3614eded383
Tomas Reinert <reinerttomas@gmail.com>
2020-01-05 20:36:30 +0100

    clear assets

---
+++ www/assets/admin.js
@@ -0,0 +1,21 @@
+/**
+ * Required dependencies
+ */
+import 'jquery';
+import 'bootstrap';
+import 'jquery.easing'
+import 'naja';
+
+/**
+ * CSS
+ * These assets are extracted to [id].bundle.css using MiniCssExtractPlugin, see more on webpack.config.js
+ */
+// import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './css/sb-admin-2.css';
+import './css/style.css';
+
+/**
+ * JS
+ */
+import './js/sb-admin-2';
