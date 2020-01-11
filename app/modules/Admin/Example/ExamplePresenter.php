d95de59e51c0857659816954600dcc42682fe35d
Tomas Reinert <reinerttomas@gmail.com>
2020-01-11 14:03:56 +0100

    example sandbox

---
+++ app/modules/Admin/Example/ExamplePresenter.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Modules\Admin\Example;
+
+use App\Modules\Admin\BaseAdminPresenter;
+use Ublaboo\DataGrid\DataGrid;
+
+class ExamplePresenter extends BaseAdminPresenter
+{
+    private array $data;
+
+    public function actionDatagrid()
+    {
+        $this->data = json_decode(file_get_contents("./table.json"), true);
+    }
+
+    protected function createComponentSimpleGrid(string $name): DataGrid
+    {
+        $grid = new DataGrid($this, $name);
+
+        $grid->setDataSource($this->data);
+        $grid->addColumnText('id', 'Id');
+        $grid->addColumnText('name', 'Name');
+        $grid->addColumnText('position', 'Position');
+        $grid->addColumnText('salary', 'Salary');
+        $grid->addColumnText('date', 'Date');
+        $grid->addColumnText('office', 'Office');
+        $grid->addColumnText('number', 'Number');
+
+        return $grid;
+    }
+}
--- app/modules/Admin/Home/HomePresenter.php
+++ app/modules/Admin/Home/HomePresenter.php
@@ -4,35 +4,8 @@ declare(strict_types=1);
 namespace App\Modules\Admin\Home;
 
 use App\Modules\Admin\BaseAdminPresenter;
-use Ublaboo\DataGrid\DataGrid;
 
 class HomePresenter extends BaseAdminPresenter
 {
-    private array $data;
 
-    /**
-     * HomePresenter constructor.
-     */
-    public function __construct()
-    {
-        $this->data = [
-            1 => ['id' => 1, 'name' => 'Tomas'],
-            2 => ['id' => 2, 'name' => 'David'],
-            3 => ['id' => 3, 'name' => 'Petr'],
-            4 => ['id' => 4, 'name' => 'Lukas'],
-            5 => ['id' => 5, 'name' => 'Iveta']
-        ];
-    }
-
-
-    protected function createComponentSimpleGrid(string $name): DataGrid
-    {
-        $grid = new DataGrid($this, $name);
-
-        $grid->setDataSource($this->data);
-        $grid->addColumnText('id', 'Id');
-        $grid->addColumnText('name', 'Name');
-
-        return $grid;
-    }
 }
