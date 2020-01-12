4c228965c1b7991d45cfd72a41a2f59ebd018b36
Tomas Reinert <reinerttomas@gmail.com>
2020-01-12 15:52:48 +0100

    add event example

---
+++ app/domain/Order/OrderLogSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Order;
+
+use App\Domain\Order\Event\OrderCreated;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+use Tracy\Debugger;
+
+class OrderLogSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            OrderCreated::class => 'log',
+        ];
+    }
+
+    public function log(Event $event): void
+    {
+        Debugger::log($event, 'info');
+        Debugger::barDump($event);
+    }
+
+}
--- app/modules/Admin/Example/ExamplePresenter.php
+++ app/modules/Admin/Example/ExamplePresenter.php
@@ -3,12 +3,18 @@ declare(strict_types=1);
 
 namespace App\Modules\Admin\Example;
 
+use App\Domain\Order\Event\OrderCreated;
 use App\Model\Utils\DateTime;
 use App\Modules\Admin\BaseAdminPresenter;
+use Nette\Application\UI\Form;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Ublaboo\DataGrid\DataGrid;
 
 class ExamplePresenter extends BaseAdminPresenter
 {
+
+    /** @inject */
+    public EventDispatcherInterface $dispatcher;
 
     private array $data;
 
@@ -37,6 +43,18 @@ class ExamplePresenter extends BaseAdminPresenter
         $grid->addColumnText('number', 'Number');
 
         return $grid;
+    }
+
+    protected function createComponentOrderForm(): Form
+    {
+        $form = new Form();
+        $form->addText('order', 'Order name')
+            ->setRequired(true);
+        $form->addSubmit('send', 'OK');
+        $form->onSuccess[] = function (Form $form): void {
+            $this->dispatcher->dispatch(new OrderCreated($form->values->order));
+        };
+        return $form;
     }
 
     public function handleRefresh(?string $snippet): void
